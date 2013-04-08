d5afa26d705a53894d1777eafaaa941987298c0f
rahulaaj <rahulajmera91@gmail.com>
2013-04-08 06:25:47 -0700

    report added and old code deleted

--- Code Old/test.py
+++
@@ -1,8 +0,0 @@
-import numpy
-import pylab
-from scipy.optimize import fmin
-
-def multi(x):
-        return (x[0]*x[0]+x[1]*x[1]+50*x[0]+100*x[1]+5)
-#multi=lambda x: (x[0]*x[0]+x[1]*x[1]+50*x[0]+100*x[1]+5)
-print list(fmin(multi,[-5,-5]))
